e9cadd567361e688353a388cc94c82fac9ce50ba
Subhash <98774745+letsbegin1234@users.noreply.github.com>
2024-06-30 22:32:38 +0530

    new

--- social_book/settings.py
+++ social_book/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@bnywoh#c*7)ym7vy@zuj$wbckhq8hjtvj=grl*h_c!w@(#4bb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost','socialweb-af6c.onrender.com']
 
 
 # Application definition
